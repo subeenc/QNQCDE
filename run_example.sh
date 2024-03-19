e23c43ca6b38ebdc35a6c8809a1734919ff963c2
Choe Subeen <103226606+subeenc@users.noreply.github.com>
2024-03-19 14:37:14 +0900

    Add files via upload

--- run_example.sh
+++ run_example.sh
@@ -5,33 +5,33 @@ echo "Start Training"
 python main.py \
   --test False \
   --max_len 50 \
-  --batch_size 256 \
+  --batch_size 4 \
   --epochs 3 \
   --eval_steps 250 \
   --lr 0.0001 \
   --warmup_ratio 0.05 \
-  --temperature 0.05 \
-  --path_to_data ./data/ \
-  --train_data train_nli_sample.tsv \
-  --valid_data valid_sts_sample.tsv
+  --temperature 0.2 \
+  --path_to_data ./data/datasets \
+  --train_data sgd_train_with_pairs.csv \
+  --valid_data sgd_clustering_dev_with_pairs.csv 
 
-echo "Start Testing"
+# echo "Start Testing"
 
-python main.py \
-  --train False \
-  --test True \
-  --max_len 50 \
-  --batch_size 256 \
-  --epochs 3 \
-  --eval_steps 250 \
-  --lr 0.00005 \
-  --warmup_ratio 0.05 \
-  --temperature 0.05 \
-  --path_to_data ./data/ \
-  --test_data test_sts_sample.tsv \
-  --path_to_saved_model output/best_checkpoint.pt
+# python main.py \
+#  --train False \
+#  --test True \
+#  --max_len 50 \
+#  --batch_size 256 \
+#  --epochs 3 \
+#  --eval_steps 250 \
+#  --lr 0.00005 \
+#  --warmup_ratio 0.05 \
+#  --temperature 0.2 \
+#  --path_to_data ./data/datasets \
+#  --test_data sgd_clustering_test_with_pairs.csv  \
+#  --path_to_saved_model output/best_checkpoint.pt
 
-echo "Semantic Search"
+#echo "Semantic Search"
 
-python SemanticSearch.py
+#python SemanticSearch.py
 
