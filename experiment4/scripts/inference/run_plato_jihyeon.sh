1abf0f0e4c08f9db9276110a0190bbd29fdf1b6d
subeenc <chltnqls19@gmail.com>
2024-04-18 11:56:08 +0900

    add experiment4

---
+++ experiment4/scripts/inference/run_plato_jihyeon.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# gpuno="0"
+gpuno="0,1"  # multi GPU 설정 
+IFS=',' read -ra ADDR <<< "$gpuno"  # gpuno 문자열을 쉼표를 구분자로 사용하여 배열로 변환
+n_gpu=${#ADDR[@]}
+
+echo "Using ${n_gpu} GPU with DDP Training."
+
+backbone='plato'
+dataset=$1
+stage='test'
+temperature=0.2
+window=2
+init_checkpoint="${backbone}.${dataset}.${temperature}t.${window}.best_model.pkl"
+# init_checkpoint="PLATO.pt"
+
+CUDA_VISIBLE_DEVICES=${gpuno} \
+# nohup python3 -u run.py \
+torchrun run.py \
+  --stage ${stage} \
+  --backbone ${backbone} \
+  --temperature ${temperature} \
+  --max_turn_view_range 10 \
+  --test_batch_size 5 \
+  --dev_batch_size 5 \
+  --use_turn_embedding True \
+  --use_role_embedding True \
+  --use_response False \
+  --sampler greedy_coreset \
+  --percentage 0.5 \
+  --window ${window} \
+  --dataset ${dataset:-"mwoz"} \
+  --init_checkpoint ${init_checkpoint:-"PLATO.pt"} \
+  --config_file "plato/config.json" \
+  --data_dir "/home/jihyeon41/research_dial_embedding/dial2vec_git/dial2vec/datasets/${dataset}"
+
+# > ./logs/dial2vec_${backbone}_${dataset}_${stage}_-1Epochs_GPU${gpuno}.log 2>&1 &
+
